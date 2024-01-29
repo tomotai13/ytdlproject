93f5a10bf34c261c1ecf03b6b73695bb2619c12e
tomotai13 <90969516+tomotai13@users.noreply.github.com>
2024-01-29 23:29:46 +0900

    Update settings.py

--- ytdlproject/settings.py
+++ ytdlproject/settings.py
@@ -39,7 +39,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #追加
 ]
+
+STATIC_URL = "static/"
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'ytdlproject.urls'
 
